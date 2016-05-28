89f5058c9456f48767f0ad5b0206662de692ade5
xinshengchou <xinshengchou@gmail.com>
2016-05-28 18:28:32 +0800

    first init

---
+++ client/src/main/java/client/EurekaClientApplication.java
@@ -0,0 +1,25 @@
+package client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by xinszhou on 5/28/16.
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
